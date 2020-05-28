ec4f9efd3596e0216d0be220226709bbcf269ea6
Anatoly Ahmedov <ludddddd@gmail.com>
2020-05-28 14:48:20 +0300

    converted to multil module gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+group = "com.ludd"
+version = "0.0.1"
+
+subprojects {
+	version = "0.0.1"
+}
+
+allprojects {
+	repositories {
+		mavenCentral()
+	}
+}
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "BackendFramework"
+include("gateway")
