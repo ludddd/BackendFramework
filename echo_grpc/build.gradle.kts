9dee063c44b79cc56d656fd5cfc2215477dc4f6d
Anatoly Ahmedov <ludddddd@gmail.com>
2020-06-04 14:25:09 +0300

    common dependencies are moved to parent project

--- echo_grpc/build.gradle.kts
+++ echo_grpc/build.gradle.kts
@@ -7,23 +7,6 @@ val spring_boot_version: String by project
 val protobuf_version: String by project
 val grpc_version: String by project
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:$spring_boot_version")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$spring_boot_version") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
-
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    implementation("com.google.protobuf:protobuf-java:3.12.2")
-    implementation("io.grpc:grpc-netty-shaded:$grpc_version")
-    implementation("io.grpc:grpc-protobuf:$grpc_version")
-    implementation("io.grpc:grpc-stub:$grpc_version")
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -10,20 +10,9 @@ val spring_boot_version: String by project
 val protobuf_version: String by project
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:$spring_boot_version")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$spring_boot_version") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-network:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
-
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    implementation("com.google.protobuf:protobuf-java:3.12.2")
 }
 
 tasks.withType<Test> {
