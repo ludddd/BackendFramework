ab26c49dcb08fc26e7aab79bb5e06fa91d6f8786
Anatoly Ahmedov <ludddddd@gmail.com>
2020-06-10 10:16:42 +0300

    echo server - do not terminate at once, wait till shutdown

--- echo_grpc/src/main/kotlin/EchoServer.kt
+++ echo_grpc/src/main/kotlin/EchoServer.kt
@@ -1,9 +1,14 @@
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import mu.KotlinLogging
+import java.util.concurrent.TimeUnit
+
+
+private val logger = KotlinLogging.logger {}
 
 class EchoServer(
     private val port: Int,
-    val server: Server) {
+    val server: Server?) {
 
     constructor(port: Int) : this(serverBuilder = ServerBuilder.forPort(port), port = port)
 
@@ -16,16 +21,41 @@ class EchoServer(
     )
 
     fun start() {
+        if (server == null) {
+            logger.error("server not initialized")
+            return
+        }
         server.start()
-        println("Server started, listening on $port")
+        logger.info("Server started, listening on $port")
+    }
+
+    @Throws(InterruptedException::class)
+    fun stop() {
+        logger.info("Stopping server")
+        server?.shutdown()?.awaitTermination(30, TimeUnit.SECONDS)
+        logger.info("Server is stopped")
+    }
+
+
+    fun awaitTermination() {
+        if (server == null) {
+            logger.error("server not initialized")
+            return
+        }
+        Runtime.getRuntime().addShutdownHook(Thread {
+            logger.info("Stopping server due to jvm shutdown")
+            stop()
+        })
+        server.awaitTermination()
     }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val port = 8980
+            val port = 9000
             val server = EchoServer(port)
             server.start()
+            server.awaitTermination()
         }
     }
 }
