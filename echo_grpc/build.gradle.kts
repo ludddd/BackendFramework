e6560e5018787ad515f6574a4724ec7669868e24
Anatoly Ahmedov <ludddddd@gmail.com>
2020-06-10 11:36:32 +0300

    configuration for integration tests

--- echo_grpc/build.gradle.kts
+++ echo_grpc/build.gradle.kts
@@ -1,3 +1,16 @@
 idea.module {
     sourceDirs.add(file(protobuf.protobuf.generatedFilesBaseDir + "/main/grpc"))
 }
+
+val integrationTest = task<Test>("integrationTest") {
+    description = "Runs integration tests."
+    group = "verification"
+
+    shouldRunAfter("test")
+
+    useJUnitPlatform() {
+        includeTags("integration")
+    }
+}
+
+tasks.check { dependsOn(integrationTest) }
