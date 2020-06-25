5f766ee9405f78fd0342e3aa44f42791072110a0
Anatoly Ahmedov <ludddddd@gmail.com>
2020-06-25 13:01:23 +0300

    dependencies are moved to common config

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,22 +31,6 @@ subprojects {
 
 	apply(plugin = "com.ludd.backend-framework")
 
-	dependencies {
-		"implementation"("org.springframework.boot:spring-boot-starter:$spring_boot_version")
-		"implementation"("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
-		"implementation"("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-		"implementation"("io.github.microutils:kotlin-logging:1.7.9")
-		"implementation"("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
-		"implementation"("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-		"implementation"("com.google.protobuf:protobuf-java:3.12.2")
-		"implementation"("io.grpc:grpc-netty-shaded:$grpc_version")
-		"implementation"("io.grpc:grpc-protobuf:$grpc_version")
-		"implementation"("io.grpc:grpc-stub:$grpc_version")
-		"testImplementation"("org.springframework.boot:spring-boot-starter-test:$spring_boot_version") {
-			exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-		}
-	}
-
 	tasks.withType<Test> {
 		useJUnitPlatform() {
 			excludeTags("integration")
