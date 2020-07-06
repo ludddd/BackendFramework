e9bacf2735aca964c1ff403874fdd61038a64f06
Anatoly Ahmedov <ludddddd@gmail.com>
2020-07-06 10:29:45 +0300

    reconnect test in progress

--- gateway/src/main/kotlin/com/ludd/gateway/ProxyRpcService.kt
+++ gateway/src/main/kotlin/com/ludd/gateway/ProxyRpcService.kt
@@ -3,10 +3,7 @@ package com.ludd.gateway
 import com.google.protobuf.ByteString
 import com.ludd.rpc.to.Message
 import io.ktor.network.selector.ActorSelectorManager
-import io.ktor.network.sockets.Socket
-import io.ktor.network.sockets.aSocket
-import io.ktor.network.sockets.openReadChannel
-import io.ktor.network.sockets.openWriteChannel
+import io.ktor.network.sockets.*
 import io.ktor.util.KtorExperimentalAPI
 import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.ByteWriteChannel
@@ -14,6 +11,9 @@ import io.ktor.utils.io.jvm.javaio.toInputStream
 import io.ktor.utils.io.jvm.javaio.toOutputStream
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 @KtorExperimentalAPI
 class ProxyRpcService(private val serviceName:String,
@@ -33,9 +33,10 @@ class ProxyRpcService(private val serviceName:String,
         return proxyConnection!!.call(arg)
     }
 
-    private fun isConnected() = proxyConnection != null
+    private fun isConnected() = proxyConnection != null && !proxyConnection!!.isClosed
 
     private suspend fun connect() {
+        logger.info("Connecting to $host:$port")
         val socket = aSocket(selectorManager).tcp().connect(host, port)
         proxyConnection = ProxyConnection(serviceName, socket)
     }
@@ -46,6 +47,9 @@ class ProxyConnection(private val serviceName: String, private val socket: Socke
 
     private val write: ByteWriteChannel = socket.openWriteChannel(autoFlush = true)
     private val read: ByteReadChannel = socket.openReadChannel()
+
+    val isClosed: Boolean
+        get() = socket.isClosed || write.isClosedForWrite || read.isClosedForRead
 
     suspend fun call(arg: ByteString): ByteString {
         val message = Message.RpcRequest
--- gateway/src/test/kotlin/com/ludd/gateway/ProxyRpcServiceProviderTest.kt
+++ gateway/src/test/kotlin/com/ludd/gateway/ProxyRpcServiceProviderTest.kt
@@ -1,16 +1,26 @@
 package com.ludd.gateway
 
+import com.google.protobuf.ByteString
 import com.ludd.rpc.EchoServer
+import com.ludd.rpc.to.Message
 import io.ktor.network.selector.ActorSelectorManager
 import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
 import io.ktor.util.KtorExperimentalAPI
+import io.ktor.utils.io.jvm.javaio.toInputStream
+import io.ktor.utils.io.jvm.javaio.toOutputStream
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import java.nio.charset.Charset
+import java.util.concurrent.TimeUnit
 
 @KtorExperimentalAPI
 @SpringBootTest(properties=[
@@ -36,15 +46,41 @@ internal class ProxyRpcServiceProviderTest {
         kotlin.test.assertEquals("aaa", response.result.toString(Charset.defaultCharset()))
     }
 
+    @Test
+    @Timeout(10, unit = TimeUnit.SECONDS)
     fun reconnect() = runBlocking {
         val selectorManager = ActorSelectorManager(Dispatchers.IO)
         val socket =  aSocket(selectorManager).tcp().connect("127.0.0.1", port = tcpServer.getPort())
+        val write = socket.openWriteChannel(autoFlush = true)
+        val read = socket.openReadChannel()
+        val text = "aaa"
 
-        GatewayServerTest.sendEchoMessage(socket, "aaa")
+        val message = Message.RpcRequest
+            .newBuilder()
+            .setService("echo")
+            .setArg(ByteString.copyFrom(text, Charset.defaultCharset()))
+            .build()
+        withContext(Dispatchers.IO) {
+            message.writeDelimitedTo(write.toOutputStream())
+        }
+
+        withContext(Dispatchers.IO) {
+            Message.RpcResponse.parseDelimitedFrom(read.toInputStream())
+        }
+
         echoServer.stop()
+        echoServer.waitTillTermination()
         echoServer.start()
+        delay(1000)
 
-        val response = GatewayServerTest.sendEchoMessage(socket, "aaa")
+        withContext(Dispatchers.IO) {
+            message.writeDelimitedTo(write.toOutputStream())
+        }
+
+        val response = withContext(Dispatchers.IO) {
+            Message.RpcResponse.parseDelimitedFrom(read.toInputStream())
+        }
+
         kotlin.test.assertNotNull(response)
         kotlin.test.assertEquals("aaa", response.result.toString(Charset.defaultCharset()))
     }
