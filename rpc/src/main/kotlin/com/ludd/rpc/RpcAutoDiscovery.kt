e8ee14f719cc838367fbf8f0b305efcc42d4cb03
Anatoly Ahmedov <ludddddd@gmail.com>
2020-07-29 14:47:07 +0300

    call method test passed

--- rpc/src/main/kotlin/com/ludd/rpc/RpcAutoDiscovery.kt
+++ rpc/src/main/kotlin/com/ludd/rpc/RpcAutoDiscovery.kt
@@ -1,26 +1,41 @@
 package com.ludd.rpc
 
 import com.google.protobuf.ByteString
-import org.reflections.Reflections
-import org.reflections.scanners.MethodAnnotationsScanner
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
-import java.lang.reflect.Method
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.callSuspend
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberFunctions
 
+data class MethodWithBoundArgument(val arg: Any, val method: KFunction<*>) {}
+
+@ExperimentalStdlibApi
 @Component
-class RpcAutoDiscovery: IRpcService {
+class RpcAutoDiscovery {
 
-    private val methodMap: Map<String, Method> by lazy {
-        val reflections = Reflections(
-            "com.ludd",
-            MethodAnnotationsScanner()
-        )
-        val methods = reflections.getMethodsAnnotatedWith(RpcMethod::class.java)
-        methods.map { it.name to it }.toMap()
+    @Autowired
+    private lateinit var context: ApplicationContext
+
+    private fun getRpcMethods(obj: Any): Map<String, MethodWithBoundArgument> {
+        val annotated = obj.javaClass.kotlin.memberFunctions.filter { it.hasAnnotation<RpcMethod>() }
+        return annotated.map { it.findAnnotation<RpcMethod>()!!.name to MethodWithBoundArgument(obj, it)}.toMap()
     }
 
-    override suspend fun call(method: String, arg: ByteString): ByteString {
-        return methodMap[method]?.let { it(arg) as ByteString }!!
+    private val methodMap: Map<String, Map<String, MethodWithBoundArgument>> by lazy {
+        val services = context.getBeansWithAnnotation(RpcService::class.java)
+        services.values.map { it.javaClass.kotlin.findAnnotation<RpcService>()!!.name to getRpcMethods(it) }.toMap()
     }
 
-    fun hasMethod(method: String) = methodMap.containsKey(method)
+    suspend fun call(service: String, method: String, arg: ByteString): ByteString {
+        val serviceMap = methodMap[service] ?: error("no service $service is found")
+        val func = serviceMap[method] ?: error("no method $method is found in service $service")
+        val rez = func.method.callSuspend(func.arg, arg)
+        return (rez as ByteString)
+    }
+
+    fun hasMethod(service: String, method: String) = methodMap.containsKey(service) && methodMap[service]!!.containsKey(method)
 }
+
--- rpc/src/test/kotlin/com/ludd/rpc/RpcMethodTest.kt
+++ rpc/src/test/kotlin/com/ludd/rpc/RpcMethodTest.kt
@@ -1,34 +1,44 @@
 package com.ludd.rpc
 
+import com.google.protobuf.ByteString
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
-import org.reflections.Reflections
-import org.reflections.scanners.MethodAnnotationsScanner
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.stereotype.Component
+import java.nio.charset.Charset
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @Component
-class RpcService {
+@RpcService(name = "serviceA")
+class ServiceA {
 
+    @Suppress("RedundantSuspendModifier")
     @RpcMethod(name = "methodA")
-    suspend fun methodA(arg: String): String {
+    suspend fun methodA(arg: ByteString): ByteString {
         return arg
     }
 }
 
+@ExperimentalStdlibApi
 @SpringBootTest
 class RpcMethodTest {
 
     @Autowired
-    private lateinit var rpcService: RpcService
+    private lateinit var rpcService: ServiceA
     @Autowired
     private lateinit var rpcAutoDiscovery: RpcAutoDiscovery
 
     @Test
     fun discoverMethod() {
-        val reflections = Reflections("com.ludd", MethodAnnotationsScanner())
-        val methods = reflections.getMethodsAnnotatedWith(RpcMethod::class.java)
-        assertTrue { rpcAutoDiscovery.hasMethod("methodA") }
+        assertTrue { rpcAutoDiscovery.hasMethod("serviceA", "methodA") }
+    }
+
+    @Test
+    fun callMethod() = runBlocking {
+        val arg = ByteString.copyFrom("aaa", Charset.defaultCharset())
+        val rez = rpcAutoDiscovery.call("serviceA","methodA", arg)
+        assertEquals(arg, rez)
     }
 }
