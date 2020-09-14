2800b4cc9d3d197933a492113398c7355000cb06
Anatoly Ahmedov <ludddddd@gmail.com>
2020-09-14 16:47:42 +0300

    correct mongo pod urls in replica set initialization

--- mongo/docker/init_replset.sh
+++ mongo/docker/init_replset.sh
@@ -4,5 +4,5 @@ until mongo mongo-0.mongo --eval "print(\"waited for connection\")"
     echo 'waiting...'
     sleep 1
   done
-mongo --eval "rs.initiate({_id:\"rs0\",members:[{_id:0,host:\"mongo-0.mongo\",priority:1.0},{_id:1,host:\"mongo-1.mongo\",priority:1.0},{_id:2,host:\"mongo-2.mongo\",priority:1.0}]})"
+mongo --eval "rs.initiate({_id:\"rs0\",members:[{_id:0,host:\"mongo-0.mongo.default.svc.cluster.local\",priority:1.0},{_id:1,host:\"mongo-1.mongo.default.svc.cluster.local\",priority:1.0},{_id:2,host:\"mongo-2.mongo.default.svc.cluster.local\",priority:1.0}]})"
 echo 'done'
