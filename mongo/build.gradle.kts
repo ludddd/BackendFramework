091af99450bd36d77f1ddf0db6d549945531fbf4
Anatoly Ahmedov <ludddddd@gmail.com>
2020-09-14 16:15:35 +0300

    add mongo replica st image to gradle build

--- mongo/build.gradle.kts
+++ mongo/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     id("io.spring.dependency-management")
     kotlin("jvm")
     kotlin("plugin.spring")
+    id("com.bmuschko.docker-remote-api") version "6.4.0"
 }
 
 group = "com.ludd"
@@ -39,4 +40,12 @@ dependencies {
 }
 
 idea.module {
+}
+
+tasks.create("buildImage", com.bmuschko.gradle.docker.tasks.image.DockerBuildImage::class) {
+    inputDir.set(file("./docker"))
+    dockerFile.set(file("./docker/primary.dockerfile"))
+    images.add("mongo-replica-set:4.4")
+    images.add("mongo-replica-set:latest")
+    group = "docker"
 }
