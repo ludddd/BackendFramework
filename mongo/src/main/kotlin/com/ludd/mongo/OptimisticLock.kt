ec306a39911a5635819e11b31057a240d5b1f447
Anatoly Ahmedov <ludddddd@gmail.com>
2020-10-02 20:40:22 +0300

    OptimisticLock - refactored to class, parameters added through class constructor

--- mongo/src/main/kotlin/com/ludd/mongo/OptimisticLock.kt
+++ mongo/src/main/kotlin/com/ludd/mongo/OptimisticLock.kt
@@ -9,19 +9,25 @@ const val MAX_LOCK_TRY = 100
 
 class OptimisticLockException(msg: String): Exception(msg)
 
-suspend fun CoroutineCollection<Document>.lock(id: ObjectId, block: (doc: Document) -> Unit) {
-    for (i in 0..MAX_LOCK_TRY) {
-        val doc: Document = findOneById(id)!!
-        block(doc)
-        var filter = Filters.eq("_id", id)
-        if (doc.containsKey("version")) {
-            filter = Filters.and(filter, Filters.eq("version", doc["version"]))
-            doc["version"] = doc.getInteger("version") + 1
-        } else {
-            doc.append("version", 0)
-            filter = Filters.and(filter, Filters.not(Filters.exists("version")))
+class OptimisticLock(private val name: String, private val maxTry: Int = MAX_LOCK_TRY) {
+    suspend fun lock(collection: CoroutineCollection<Document>, id: ObjectId, block: (doc: Document) -> Unit) {
+        for (i in 0..maxTry) {
+            val doc: Document = collection.findOneById(id)!!
+            block(doc)
+            var filter = Filters.eq("_id", id)
+            if (doc.containsKey("version")) {
+                filter = Filters.and(filter, Filters.eq("version", doc["version"]))
+                doc["version"] = doc.getInteger("version") + 1
+            } else {
+                doc.append("version", 0)
+                filter = Filters.and(filter, Filters.not(Filters.exists("version")))
+            }
+            if (collection.replaceOne(filter, doc).matchedCount == 1L) return
         }
-        if (replaceOne(filter, doc).matchedCount == 1L) return
+        throw OptimisticLockException("Failed to acquire lock $name")
     }
-    throw OptimisticLockException("Failed to acquire lock for document $id in collection $namespace")
+}
+
+suspend fun CoroutineCollection<Document>.lock(id: ObjectId, block: (doc: Document) -> Unit) {
+    OptimisticLock("for document $id in collection $namespace").lock(this, id, block)
 }
