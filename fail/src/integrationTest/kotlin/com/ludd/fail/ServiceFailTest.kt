79df6009ea77b8c00353a99c343bf4c346a69bea
Anatoly Ahmedov <ludddddd@gmail.com>
2020-10-18 15:55:27 +0300

    fail service - restart after crash test addition logging

--- fail/src/integrationTest/kotlin/com/ludd/fail/ServiceFailTest.kt
+++ fail/src/integrationTest/kotlin/com/ludd/fail/ServiceFailTest.kt
@@ -3,6 +3,7 @@ package com.ludd.fail
 import com.google.protobuf.ByteString
 import com.ludd.rpc.TestClient
 import com.ludd.rpc.to.Message
+import com.ludd.test_utils.KGenericContainer
 import io.ktor.util.*
 import io.kubernetes.client.openapi.ApiClient
 import io.kubernetes.client.openapi.Configuration
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.nio.charset.Charset
+import java.time.Duration
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -38,14 +40,35 @@ class ServiceFailTest {
         assertEquals("aaa", response.result.toString(Charset.defaultCharset()))
     }
 
+    private val fail = KGenericContainer("ludd.fail:0.1")
+        .withExposedPorts(9001)
+        .withStartupTimeout(Duration.ofMinutes(5))
+
+    @Test
+    fun crash() = runBlocking {
+        fail.start()
+        val port = fail.firstMappedPort
+        val client = TestClient("localhost", port)
+        val req = Message.InnerRpcRequest.newBuilder()
+            .setService("fail")
+            .setMethod("crash")
+            .build()
+        client.send(req)
+        fail.stop()
+    }
+
     @Test
     @Timeout(1, unit = TimeUnit.MINUTES)
     fun restartAfterCrash() = runBlocking{
         val client = TestClient()
         client.sendRpc("fail", "echo", ByteString.copyFromUtf8("aaa"))
         client.receive(Message.RpcResponse::parseDelimitedFrom)
-        client.sendRpc("fail", "crash", ByteString.copyFromUtf8(""))
+        client.sendRpc("fail",  "crash", ByteString.copyFromUtf8(""))
         val response = client.receive(Message.RpcResponse::parseDelimitedFrom)
+        if (response.hasError) {
+            logger.info(response.error)
+        }
+        assertFalse(response.hasError)
         assertEquals("bbb", response.result.toString(Charset.defaultCharset()))
     }
 
