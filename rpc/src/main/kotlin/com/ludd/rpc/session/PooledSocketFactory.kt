91f11f29772f17d606210dbcb2d35b5270d638fa
Anatoly Ahmedov <ludddddd@gmail.com>
2020-10-20 16:57:51 +0300

    obsolete member removed

--- rpc/src/main/kotlin/com/ludd/rpc/session/PooledSocketFactory.kt
+++ rpc/src/main/kotlin/com/ludd/rpc/session/PooledSocketFactory.kt
@@ -4,13 +4,8 @@ import com.ludd.rpc.conn.ConnectionPool
 import com.ludd.rpc.conn.RpcSocket
 import com.ludd.rpc.conn.RpcSocketFactory
 import com.ludd.rpc.conn.SocketWrapperFactory
-import io.ktor.network.selector.*
-import io.ktor.util.*
-import kotlinx.coroutines.Dispatchers
 
 class PooledSocketFactory(private val poolSize:Int) : RpcSocketFactory {
-    @OptIn(KtorExperimentalAPI::class)
-    private val selectorManager = ActorSelectorManager(Dispatchers.IO)
     private val poolMap = mutableMapOf<Pair<String, Int>, ConnectionPool>()
     private val socketFactory = SocketWrapperFactory()
 
