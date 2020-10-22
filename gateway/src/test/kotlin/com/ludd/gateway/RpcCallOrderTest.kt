5aa5b0fd113a7823a4a30149671583253671329d
Anatoly Ahmedov <ludddddd@gmail.com>
2020-10-22 09:40:25 +0300

    multipleCallAtOnce - extract some code to method

--- gateway/src/test/kotlin/com/ludd/gateway/RpcCallOrderTest.kt
+++ gateway/src/test/kotlin/com/ludd/gateway/RpcCallOrderTest.kt
@@ -7,10 +7,7 @@ import com.ludd.rpc.SessionContext
 import com.ludd.rpc.conn.SocketWrapperFactory
 import com.ludd.rpc.session.SocketSession
 import io.ktor.util.*
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.newFixedThreadPoolContext
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import mu.KotlinLogging
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers
@@ -62,16 +59,7 @@ class RpcCallOrderTest {
             val barrier = Job()
 
             val jobs = data.map {
-                async(threadPool) {
-                    barrier.join()
-                    val rez = service.call(
-                        "test",
-                        it.toByteArray(Charset.defaultCharset()),
-                        SessionContext(InetSocketAddress(0))
-                    )
-                    val resp = rez.result?.toString(Charset.defaultCharset())
-                    resp
-                }
+                callRpcAsync(threadPool, barrier, service, it)
             }
             barrier.complete()
             val rez = jobs.map { it.await() }
@@ -79,6 +67,25 @@ class RpcCallOrderTest {
         }
 
         server.stop()
+    }
+
+    @Suppress("EXPERIMENTAL_API_USAGE")
+    private fun CoroutineScope.callRpcAsync(
+        threadPool: ExecutorCoroutineDispatcher,
+        barrier: CompletableJob,
+        service: ProxyRpcService,
+        arg: String
+    ): Deferred<String?> {
+        return async(threadPool) {
+            barrier.join()
+            val rez = service.call(
+                "test",
+                arg.toByteArray(Charset.defaultCharset()),
+                SessionContext(InetSocketAddress(0))
+            )
+            val resp = rez.result?.toString(Charset.defaultCharset())
+            resp
+        }
     }
 
 }
